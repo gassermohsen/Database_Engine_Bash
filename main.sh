c7b6cab6f1f8be7e8d32a84e300210dcb901fcf7
Gasser Mohsen <gassermohsen83@gmail.com>
2022-12-27 18:52:52 +0200

    main script

---
+++ main.sh
@@ -0,0 +1,67 @@
+
+
+#make database directory if not exist 
+mkdir -p database 
+cd database 
+
+
+
+
+
+function selection(){
+
+    options=("Create-Database" "List-all-Databases" "Drop-Database" "Connect-Database" "Exit")
+
+select opt in "${options[@]}"
+do 
+case $opt in
+"Create-Database") 
+source .././createdatabase.sh
+break;
+;;
+"List-all-Databases")
+source .././listdatabases.sh
+selection
+break
+ 
+;;
+"Drop-Database")
+source .././dropdatabase.sh
+break
+
+;;
+"Connect-Database")
+source .././connectdatabase.sh
+break
+;;
+Exit)
+echo "Exit" 
+break;
+
+;;
+esac
+done
+   
+}
+
+selection
+
+
+
+# function createdatabase(){
+
+#     read -p "Please enter database name " dir
+
+# if [[ ! -e $dir ]]; then
+#     mkdir $dir
+#     cd $dir 
+#     echo "$dir Database created succesfully "
+# else  
+# echo "-----------------------------------------------------"
+
+# echo "$dir database already exists"
+# echo "-------------------------------------------------"
+# clear
+# selection
+# fi    
+# }
